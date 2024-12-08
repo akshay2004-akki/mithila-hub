0ed05f152daa7c2e4f93f6058e1f69c85daed088
Akshay anand <akshay1622004@gmail.com>
2024-12-08 23:40:31 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <div className='peer'>Hover</div>
+      <div className='peer-hover:text-yellow-700 peer-hover:bg-black sm:text-2xl text-9xl'>Akshay</div>
+    </>
+  )
+}
+
+export default App
