a14699cd59513e4b1ce2abc82b2d0a1fe8d782ff
Akshay anand <akshay1622004@gmail.com>
2024-12-10 03:26:20 +0530

    starting

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: '#faf7f0',
+        maroon: '#a74b41',
+        gold: '#d4af37',
+    },
+    },
   },
   plugins: [],
 }
