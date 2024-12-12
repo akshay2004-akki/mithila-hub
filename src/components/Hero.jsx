dc17683e9a089b83b6eb1296cf386876346bfe04
Akshay anand <akshay1622004@gmail.com>
2024-12-12 20:28:43 +0530

    update hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,11 @@ import img1 from "../assets/border.jpeg";
 import img2 from "../assets/fish.webp";
 import img3 from "../assets/kobhar.png";
 import img4 from "../assets/new.jpg";
+import img5 from "../assets/DALL·E 2024-12-11 20.35.44 - A Mithila painting consisting of only four thin borders, one on each side of a white canvas. The borders feature intricate but minimal geometric patte.webp";
+import img6 from "../assets/pic.jpg";
+import img7 from "../assets/DALL·E 2024-12-12 19.19.20 - A minimalist Mithila painting style artwork featuring traditional motifs such as peacocks, fish, and intricate floral patterns. The design should be l.webp";
+import img8 from "../assets/DALL·E 2024-12-12 19.20.17 - A lightweight Mithila painting artwork with a modern minimalist touch. The design includes traditional motifs like elephants, fish, and intricate geom.webp";
+
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -21,35 +26,39 @@ function Hero() {
   };
   return (
     <>
-      <section
-        className="relative h-[100vh] bg-cover bg-center flex items-center justify-center sm:flex-row"
-        style={{ backgroundImage: "url(https://example.com/banner.jpg)" }}
-      >
-        <div className="p-6 rounded-lg text-left">
-          <h1 className="text-4xl text-left font-bold text-black mb-2">
-            Authentic Mithila Art
-          </h1>
-          <p className="text-lg text-left text-black mb-4">
-            Bringing tradition and beauty to your home
-          </p>
-          <button className="bg-gold mx-0 text-white py-2 px-4 rounded hover:bg-yellow-600">
-            Explore Gallery
-          </button>
+      <section className="relative w-full h-screen translate-y-[72px]">
+        <div className="flex flex-col sm:flex-row h-screen w-full gap-[1px]">
+          <img
+            src={img7}
+            alt=""
+            className="object-cover brightness-[0.6] h-[50vh] sm:h-full w-full sm:w-1/2"
+          />
+
+          <img
+            src={img8}
+            alt=""
+            className="object-cover brightness-[0.6] h-[50vh] sm:h-full w-full sm:w-1/2"
+          />
         </div>
 
-        <div className="w-56 max-w-4xl mx-auto mt-10">
+        <img
+          src={img6}
+          alt=""
+          className="absolute h-[200px] w-[200px] sm:h-[250px] sm:w-[250px] rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-white brightness-125"
+        />
+        {/* <div className="w-56 translate-y-[30px] sm:translate-y-[50px] md:w-64">
           <Slider {...settings}>
             {images.map((image, index) => (
-              <div key={index} className="px-2">
+              <div key={index} className="w-full flex justify-center items-center">
                 <img
                   src={image}
                   alt={`Slide ${index + 1}`}
-                  className="rounded-lg shadow-lg object-cover h-80"
+                  className="rounded-lg shadow-lg object-cover h-72 w-96"
                 />
               </div>
             ))}
           </Slider>
-        </div>
+        </div> */}
       </section>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,177 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/image.png";
 
 const Navbar = () => {
-    const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
+  const [toggle2, setToggle2] = useState(false);
+  const [toggle3, setToggle3] = useState(false);
+
   return (
     <>
-        <div className="w-full fixed font-sans bg-beige text-gray-900">
-            {/* Header */}
-            <header className="bg-maroon px-3 py-5 shadow-lg">
-                <div className="flex justify-between mx-6 items-center">
-                    <div className="text-2xl font-serif text-white font-thin">Mithila Creations</div>
-                    <nav>
-                        <ul className="hidden space-x-8 text-[18px] sm:flex">
-                            <li><Link to="/" className="text-white hover:text-gold">Home</Link></li>
-                            <li><Link to="/gallery" className="text-white hover:text-gold">Gallery</Link></li>
-                            <li><Link to="/about" className="text-white hover:text-gold">About Us</Link></li>
-                            <li> <Link to="/contact" className="text-white hover:text-gold">Contact Us</Link></li>
-                        </ul>
-                        <div className="block sm:hidden">
-                            <button onClick={()=>setToggle(prev=>!prev)} className='font-bold text-white text-2xl transition'><i className={`${toggle ? "fa-solid fa-x" : "fa-solid fa-bars"}`}></i></button>
-                        </div>
-                        <div className={`z-10 absolute py-2 px-4 rounded right-6 bg-black flex flex-col ${toggle ? "block " : "hidden"}`}>
-                            <Link to="/" className="text-white hover:text-gold p-2">Home</Link>
-                            <Link to="/gallery" className="text-white hover:text-gold p-2">Gallery</Link>
-                            <Link to="/about" className="text-white hover:text-gold p-2">About Us</Link>
-                            <Link to="/contact" className="text-white hover:text-gold p-2">Contact Us</Link>
-                        </div>
+      <div className="w-full fixed z-10 font-sans bg-beige text-gray-900">
+        {/* Header */}
+        <header className="bg-white py-3 shadow-lg">
+          <div className="flex justify-between mx-6 items-center">
+            <div className="text-2xl font-serif text-black font-thin">
+              <img src={logo} className="h-[60px] w-[60px] rounded-full" />
+            </div>
+            <nav>
+              <ul className="hidden space-x-8 text-[18px] sm:flex">
+                <li>
+                  <Link to="/" className="text-black hover:text-gold">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/gallery" className="text-black hover:text-gold">
+                    Gallery
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/about" className="text-black hover:text-gold">
+                    About Us
+                  </Link>
+                </li>
+                <li className="text-black hover:text-gold">
+                  <button onClick={() => setToggle3((prev) => !prev)}>
+                    Categories
+                  </button>
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/contact" className="text-black hover:text-gold">
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
 
-                    </nav>
-                </div>
-            </header>
+              <div
+                className={`absolute right-[150px] py-2 px-4 bg-black flex flex-col top-16 rounded text-black ${
+                  toggle3 ? "block" : "hidden"
+                }`}
+              >
+                <button onClick={() => setToggle3((prev) => !prev)}>
+                  <i className="fa-solid fa-x absolute right-2 text-white"></i>
+                </button>
+                <Link
+                  to="/sarees"
+                  className="py-3 text-slate-500 hover:text-white"
+                >
+                  Saree
+                </Link>
+                <Link
+                  to="/chunri"
+                  className="py-3 text-slate-500 hover:text-white"
+                >
+                  Chunari
+                </Link>
+                <Link
+                  to="/bedsheet"
+                  className="py-3 text-slate-500 hover:text-white"
+                >
+                  Bed Sheet
+                </Link>
+                <Link
+                  to="/canvas"
+                  className="py-3 text-slate-500 hover:text-white"
+                >
+                  Canvas
+                </Link>
+                <Link
+                  to="/custom"
+                  className="py-3 text-[15px] text-slate-500 hover:text-white"
+                >
+                  Customization
+                </Link>
+              </div>
 
-            
-        </div>
+              <div className="block sm:hidden">
+                <button
+                  onClick={() => setToggle((prev) => !prev)}
+                  className="font-bold text-black text-2xl transition"
+                >
+                  <i
+                    className={`${
+                      toggle ? "fa-solid fa-x" : "fa-solid fa-bars"
+                    }`}
+                  ></i>
+                </button>
+              </div>
+              <div
+                className={`z-10 absolute py-2 px-4 rounded right-6 bg-black flex flex-col ${
+                  toggle ? "block " : "hidden"
+                }`}
+              >
+                <Link to="/" className="text-white hover:text-gold p-2">
+                  Home
+                </Link>
+                <Link to="/gallery" className="text-white hover:text-gold p-2">
+                  Gallery
+                </Link>
+                <Link to="/about" className="text-white hover:text-gold p-2">
+                  About Us
+                </Link>
+                <button
+                  className="text-white hover:text-gold peer p-2"
+                  onClick={() => setToggle2((prev) => !prev)}
+                >
+                  Categories
+                </button>
+                <Link to="/contact" className="text-white hover:text-gold p-2">
+                  Contact Us
+                </Link>
+              </div>
+              <div
+                className={`absolute right-[150px] py-2 px-4 bg-black flex flex-col sm:hidden top-48 rounded ${
+                  toggle2 ? "block" : "hidden"
+                }`}
+              >
+                <button onClick={() => setToggle2((prev) => !prev)}>
+                  <i className="fa-solid fa-x absolute right-2 text-white"></i>
+                </button>
+                <Link
+                  to="/sarees"
+                  className="py-3 text-slate-300 hover:text-white"
+                  onClick={()=>{setToggle(false); setToggle2(false); setToggle3(false)}}
+                >
+                  Saree
+                </Link>
+                <Link
+                  to="/chunri"
+                  className="py-3 text-slate-300 hover:text-white"
+                  onClick={()=>{setToggle(false); setToggle2(false); setToggle3(false)}}
+                >
+                  Chunari
+                </Link>
+                <Link
+                  to="/bedsheet"
+                  className="py-3 text-slate-300 hover:text-white"
+                  onClick={()=>{setToggle(false); setToggle2(false); setToggle3(false)}}
+                >
+                  Bed Sheet
+                </Link>
+                <Link
+                  to="/canvas"
+                  className="py-3 text-slate-300 hover:text-white"
+                  onClick={()=>{setToggle(false); setToggle2(false); setToggle3(false)}}
+                >
+                  Canvas
+                </Link>
+                <Link
+                  to="/custom"
+                  className="py-3 text-[15px] text-slate-300 hover:text-white"
+                  onClick={()=>{setToggle(false); setToggle2(false); setToggle3(false)}}
+                >
+                  Customization
+                </Link>
+              </div>
+            </nav>
+          </div>
+        </header>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
