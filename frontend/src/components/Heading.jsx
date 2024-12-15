394eda73f1e28715c16dd9c8eb0a58762aba65eb
Akshay anand <akshay1622004@gmail.com>
2024-12-15 16:37:59 +0530

    heading

--- frontend/src/components/Heading.jsx
+++ frontend/src/components/Heading.jsx
@@ -3,7 +3,7 @@
 function Heading({heading, psudo}) {
   return (
     <h2 className={` relative text-3xl font-bold font-poppins text-center mb-12 before:content-['${psudo}'] before:absolute before:text-maroon before:text-[13px] md:before:text-[15px] before:top-[-20px] before:left-1/2 before:-translate-x-1/2 before:translate-y-[50px]`}>
-      <span className="relative z-10 text-2xl md:text-3xl px-4 bg-white">
+      <span className="relative z-10 text-2xl md:text-3xl px-4 bg-slate-100">
         {heading}
       </span>
       <span className="absolute inset-0 flex items-center">
